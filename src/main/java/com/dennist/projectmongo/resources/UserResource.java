6a978ec503be6855c3951de648aef65a085341dc
dennis <dennis.teles@gmail.com>
2022-07-19 18:18:57 -0300

    Entity User and REST funcionando

---
+++ src/main/java/com/dennist/projectmongo/resources/UserResource.java
@@ -0,0 +1,25 @@
+package com.dennist.projectmongo.resources;
+
+import com.dennist.projectmongo.domain.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<List<User>> findAll() {
+        User maria = new User("1", "Maria Brown", "maria@gmail.com");
+        User alex = new User("2", "Alex Green", "alex@gmail.com");
+        List<User> list = new ArrayList<>(Arrays.asList(maria, alex));
+        return ResponseEntity.ok().body(list);
+    }
+
+}
