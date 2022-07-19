1bc7d851de8b724f2594845e46858ad288dc1e6e
dennis <dennis.teles@gmail.com>
2022-07-19 16:19:22 -0300

    First commit

---
+++ src/main/java/com/dennist/projectmongo/ProjectmongoApplication.java
@@ -0,0 +1,13 @@
+package com.dennist.projectmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectmongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectmongoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dennist/projectmongo/ProjectmongoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennist.projectmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectmongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
