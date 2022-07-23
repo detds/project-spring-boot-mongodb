b19db6acfe46747b0883479cf908e5c024056f98
dennis <dennis.teles@gmail.com>
2022-07-23 17:24:13 -0300

    Obtendo usuario por id

---
+++ src/main/java/com/dennist/projectmongo/services/exception/ObjectNotFoundException.java
@@ -0,0 +1,12 @@
+package com.dennist.projectmongo.services.exception;
+
+import java.io.Serial;
+
+public class ObjectNotFoundException extends RuntimeException {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    public ObjectNotFoundException(String msg) {
+        super(msg);
+    }
+}
