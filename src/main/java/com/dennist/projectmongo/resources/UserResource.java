c4e1a1f4cf854d89320aa4a933daeff285700d8b
dennis <dennis.teles@gmail.com>
2022-07-26 18:25:10 -0300

    Atualizacao de usuario com PUT

--- src/main/java/com/dennist/projectmongo/resources/UserResource.java
+++ src/main/java/com/dennist/projectmongo/resources/UserResource.java
@@ -46,4 +46,12 @@ public class UserResource {
         return ResponseEntity.noContent().build();
     }
 
+    @PutMapping(value = "/{id}")
+    public ResponseEntity<Void> update(@RequestBody UserDTO objDto, @PathVariable String id) {
+        User obj = service.fromDTO(objDto);
+        obj.setId(id);
+        obj = service.update(obj);
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/dennist/projectmongo/services/UserService.java
+++ src/main/java/com/dennist/projectmongo/services/UserService.java
@@ -34,6 +34,17 @@ public class UserService {
         repo.deleteById(id);
     }
 
+    public User update(User obj) {
+        User newObj = repo.findById(obj.getId()).orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+        updateData(newObj, obj);
+        return repo.save(newObj);
+    }
+
+    private void updateData(User newObj, User obj) {
+        newObj.setName(obj.getName());
+        newObj.setEmail(obj.getEmail());
+    }
+
     public User fromDTO(UserDTO objDto) {
         return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
