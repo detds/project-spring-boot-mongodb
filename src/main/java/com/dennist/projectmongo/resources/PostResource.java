1e553638834f4cad01b499a2f5d0003c34ed47d2
dennis <dennis.teles@gmail.com>
2022-07-28 17:16:43 -0300

    Obtendo post por ID

---
+++ src/main/java/com/dennist/projectmongo/resources/PostResource.java
@@ -0,0 +1,24 @@
+package com.dennist.projectmongo.resources;
+
+import com.dennist.projectmongo.domain.Post;
+import com.dennist.projectmongo.services.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/posts")
+public class PostResource {
+
+    @Autowired
+    private PostService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Post> findById(@PathVariable String id) {
+        Post obj = service.findById(id);
+        return ResponseEntity.ok().body(obj);
+    }
+}
