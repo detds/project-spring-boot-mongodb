94b31102f559eed50edc45b8a57bd132a0a2042f
dennis <dennis.teles@gmail.com>
2022-07-28 17:05:14 -0300

    Endpoint para retornar os posts de um usuario

--- src/main/java/com/dennist/projectmongo/resources/UserResource.java
+++ src/main/java/com/dennist/projectmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.dennist.projectmongo.resources;
 
+import com.dennist.projectmongo.domain.Post;
 import com.dennist.projectmongo.domain.User;
 import com.dennist.projectmongo.dto.UserDTO;
 import com.dennist.projectmongo.services.UserService;
@@ -52,6 +53,12 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @GetMapping(value = "/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 
 }
