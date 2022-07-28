9155f500a025301f5f37d61720479f98943de7fe
dennis <dennis.teles@gmail.com>
2022-07-28 16:35:19 -0300

    Projecao dos dados do autor com DTO

--- src/main/java/com/dennist/projectmongo/config/Instantiation.java
+++ src/main/java/com/dennist/projectmongo/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.dennist.projectmongo.config;
 
 import com.dennist.projectmongo.domain.Post;
 import com.dennist.projectmongo.domain.User;
+import com.dennist.projectmongo.dto.AuthorDTO;
 import com.dennist.projectmongo.repository.PostRepository;
 import com.dennist.projectmongo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -34,10 +35,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordeu feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordeu feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
 
     }
