264651774213fd9528d1b02a93f4811e2d3ec180
detds <dennis.teles@gmail.com>
2022-11-27 19:00:14 -0300

    fixed GetMapping values of PostResource

--- src/main/java/com/dennist/projectmongo/resources/PostResource.java
+++ src/main/java/com/dennist/projectmongo/resources/PostResource.java
@@ -23,14 +23,14 @@ public class PostResource {
         return ResponseEntity.ok().body(obj);
     }
 
-    @GetMapping(value = "titlesearch")
+    @GetMapping(value = "/titlesearch")
     public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text) {
         text = URL.decodeParam(text);
         List<Post> list = service.findByTitle(text);
         return ResponseEntity.ok().body(list);
     }
 
-    @GetMapping(value = "fullsearch")
+    @GetMapping(value = "/fullsearch")
     public ResponseEntity<List<Post>> fullSearch(
             @RequestParam(value = "text", defaultValue = "") String text,
             @RequestParam(value = "minDate", defaultValue = "") String minDate,
